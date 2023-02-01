86b1940b6d0c800aa7c9e9c0989721172fdfa82e
Kartikey Ranjan <kartranj@amazon.co.uk>
2023-02-01 12:16:54 +0000

    fixed route

--- js/form.js
+++ js/form.js
@@ -2,10 +2,15 @@ function validateForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username == "demo" && password == "pass") {
+    if (username == "james@uotagooro.co.uk" && password == "pass") {
         window.location.href = "../api/student.html";
         return false;
-    } else {
+    }
+    else if (username == "charles@uotagooro.co.uk" && password == "pass") {
+        window.location.href = "../api/students.html"
+        return false;
+    }
+    else {
         alert("Incorrect username or password");
         return false;
     }
